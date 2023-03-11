5c8b19e26eaecb357537b951ea5f9f9e0c2b2a77
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-03-11 00:57:08 +0000

    Mar 10, 2023, 7:57 PM

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject zoomCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
