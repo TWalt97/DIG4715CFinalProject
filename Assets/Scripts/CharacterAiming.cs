5792522ef913bb4e506feb23578860de6a932922
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-04-02 18:56:49 -0400

    update
    changed camera
    freelook for unzoomed
    changes to virtual cam when zoomed
    
    still needs tweaking

--- Assets/Scripts/CharacterAiming.cs
+++ Assets/Scripts/CharacterAiming.cs
@@ -10,6 +10,8 @@ public class CharacterAiming : MonoBehaviour
     public Cinemachine.AxisState xAxis;
     public Cinemachine.AxisState yAxis;
 
+    public GameObject zoomCam;
+
     Camera mainCamera;
     // Start is called before the first frame update
     void Start()
@@ -22,12 +24,16 @@ public class CharacterAiming : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        xAxis.Update(Time.fixedDeltaTime);
-        yAxis.Update(Time.fixedDeltaTime);
+        if (zoomCam.activeSelf == true)
+        {
+            xAxis.Update(Time.fixedDeltaTime);
+            yAxis.Update(Time.fixedDeltaTime);
 
-        cameraLookAt.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
+            cameraLookAt.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
 
-        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+            float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+        }
+        
     }
 }
