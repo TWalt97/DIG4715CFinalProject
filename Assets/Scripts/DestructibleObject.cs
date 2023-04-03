4f1a59891edff349555064c632f81bbd904c228f
Avery Carpenter <avercarpenter@gmail.com>
2023-04-02 23:14:08 -0400

    particle

--- Assets/Scripts/DestructibleObject.cs
+++ Assets/Scripts/DestructibleObject.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class DestructibleObject : MonoBehaviour
 {
+  
     public float health = 5f;
+    public ParticleSystem destroyEffect; 
+   
+
     public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0f)
         {
+            Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            destroyEffect.Play();
             Die();
         }
     }
