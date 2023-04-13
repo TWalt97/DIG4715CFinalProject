f96892ef6c4c433020782cd83575e523f6ae8aad
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2023-04-13 14:57:23 -0400

    Working on HUD & HUD Code

--- Assets/Scripts/HUD Scripts/HUD.cs
+++ Assets/Scripts/HUD Scripts/HUD.cs
@@ -6,24 +6,40 @@ using TMPro;
 
 public class HUD : MonoBehaviour
 {
-    // public PlayerController player;
+    // Testing
+    public bool isHub;
+    public bool isMaze;
+    public bool isArena;
+    public bool isVent;
 
+    private bool isGlow;
     private bool isAiming = false;
-    private bool isCooldown = false;
 
-    public float glowCooldownTime = 10.0f; 
-    public float abilityCooldownTime = 20.0f;
+    // Old Glow
+    // private bool isCooldown = false;
 
-    private float glowCooldownTimer = 0.0f;
-    private float abilityCooldownTimer = 0.0f;
-    // private float charWidth = 0.3f;
+    // public float glowCooldownTime = 10.0f; 
+    // public float abilityCooldownTime = 20.0f;
 
-    [SerializeField]
-    private TextMeshProUGUI levelTimerText;
-    [SerializeField]
-    private TextMeshProUGUI glowAbilityTimerText;
-    [SerializeField]
-    private TextMeshProUGUI glowCooldownTimerText;
+    // private float glowCooldownTimer = 0.0f;
+    // private float abilityCooldownTimer = 0.0f;
+
+    // [SerializeField]
+    // private TextMeshProUGUI glowAbilityTimerText;
+    // [SerializeField]
+    // private TextMeshProUGUI glowCooldownTimerText;
+
+    // public Button mazeObjectiveButtonBorder;
+    // public Button mazeObjectiveButtonBackground;
+    // public TextMeshProUGUI mazeObjectiveText;
+
+    // public Button arenaObjectiveButtonBorder;
+    // public Button arenaObjectiveButtonBackground;
+    // public TextMeshProUGUI arenaObjectiveText;
+    
+    // public Button ventObjectiveButtonBorder;
+    // public Button ventObjectiveButtonBackground;
+    // public TextMeshProUGUI ventObjectiveText;
 
     public Button shrinkButtonBorder;
     public Button shrinkButtonBackground;
@@ -37,6 +53,30 @@ public class HUD : MonoBehaviour
     public Button glowButtonBorder;
     public Button glowButtonBackground;
 
+    // private ColorBlock shrinkBorderColors = shrinkButtonBorder.colors;
+    // private ColorBlock shrinkBackgroundColors = shrinkButtonBackground.colors;
+    // private ColorBlock aimBorderColors = aimButtonBorder.colors;
+    // private ColorBlock aimBackgroundColors = arenaObjectiveButtonBackground.colors;
+    // private ColorBlock laserBorderColors = laserButtonBorder.colors;
+    // private ColorBlock laserBackgroundColors = laserButtonBackground.colors;
+    // private ColorBlock glowBorderColors = glowButtonBorder.colors;
+    // private ColorBlock glowBackgroundColors = glowButtonBackground.colors;
+
+    // private Color mazeBorderButtonBaseColor = new Color(0.2470588f, 0.2392157f, 0.5882353f, 1.0f);
+    // private Color mazeBorderButtonPressedColor = new Color(0.2470588f, 0.2392157f, 0.5882353f, 0.5882353f);
+    // private Color mazeBackgroundButtonBaseColor = new Color (0.6196079f, 0.7301583f, 0.8980392f, 1.0f);
+    // private Color mazeBackgroundButtonPressedColor = new Color (0.6196079f, 0.7301583f, 0.8980392f, 0.5882353f);
+
+    // private Color arenaBorderButtonBaseColor = new Color(0.5843138f, 0.2352941f, 0.2388183f, 1.0f);
+    // private Color arenaBorderButtonPressedColor = new Color(0.5843138f, 0.2352941f, 0.2388183f, 0.5882353f);
+    // private Color arenaBackgroundButtonBaseColor = new Color(0.8980392f, 0.6875842f, 0.6196079f, 1.0f);
+    // private Color arenaBackgroundButtonPressedColor = new Color(0.8980392f, 0.6875842f, 0.6196079f, 0.5882353f);
+
+    // private Color ventBorderButtonBaseColor = new Color(0.8901961f, 0.8980393f, 0.6196079f, 1.0f);
+    // private Color ventBorderButtonPressedColor = new Color(0.8901961f, 0.8980393f, 0.6196079f, 0.5882353f);
+    // private Color ventBackgroundButtonBaseColor = new Color(0.8161573f, 0.8301887f, 0.3320754f, 1.0f);
+    // private Color ventBackgroundButtonPressedColor = new Color(0.8161573f, 0.8301887f, 0.3320754f, 0.5882353f);
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -47,8 +87,8 @@ public class HUD : MonoBehaviour
 
         glowButtonBorder.interactable = true;
         glowButtonBackground.interactable = true;
-        glowAbilityTimerText.gameObject.SetActive(false);
-        glowCooldownTimerText.gameObject.SetActive(false);
+        // glowAbilityTimerText.gameObject.SetActive(false);
+        // glowCooldownTimerText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -105,23 +145,17 @@ public class HUD : MonoBehaviour
             FadeToColor(laserButtonBackground.colors.normalColor, laserButtonBackground);
         }
 
-        // Glowing
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            UseGlow();
-        }
+        // Old Glowing
+        // if (Input.GetKeyDown(KeyCode.E))
+        // {
+        //     UseGlow();
+        // }
 
-        if (isCooldown)
-        {
-            ApplyCooldown();
-        }
+        // if (isCooldown)
+        // {
+        //     ApplyCooldown();
+        // }
     }
-
-    // void CenterText()
-    // {
-    //     string levelTimer = player.timer.toString("00.00");
-    //     levelTimerText.SetText($"<mspace={charWidth}em>{levelTimer}");
-    // }
 
     void UseAbility(Button button)
     {
@@ -135,47 +169,77 @@ public class HUD : MonoBehaviour
         graphic.CrossFadeColor(color, button.colors.fadeDuration, true, true);
     }
 
-    void UseGlow()
+    public void isGlowing (bool isGlow)
     {
-        if (!isCooldown)
+        if (isGlow)
         {
-            isCooldown = true;
-            glowAbilityTimerText.gameObject.SetActive(true);
             glowButtonBorder.interactable = false;
             glowButtonBackground.interactable = false;
-            glowCooldownTimer = glowCooldownTime;
-            abilityCooldownTimer = abilityCooldownTime;
-        }
-
-    }
-
-    void ApplyCooldown()
-    {
-        abilityCooldownTimer -= Time.deltaTime;
-        glowCooldownTimer -= Time.deltaTime;
-
-        if (abilityCooldownTimer < 0.0f)
-        {
-            glowAbilityTimerText.gameObject.SetActive(false);
-            glowCooldownTimerText.gameObject.SetActive(true);
-            
-            if (glowCooldownTimer < 0.0f)
-            {
-                isCooldown = false;
-                glowCooldownTimerText.gameObject.SetActive(false);
-                glowButtonBorder.interactable = true;
-                glowButtonBackground.interactable = true;
-            }
-            else
-            {
-                glowCooldownTimerText.text = Mathf.RoundToInt(glowCooldownTimer).ToString();
-            }
         }
         else
         {
-            glowAbilityTimerText.text = Mathf.RoundToInt(abilityCooldownTimer).ToString();
+            glowButtonBorder.interactable = true;
+            glowButtonBackground.interactable = true;
         }
-        
-        
     }
+
+    // public void ChangeHUDDefault (bool isHub)
+    // {
+
+    // }
+
+    public void ChangeObjective (String level)
+    {
+        if (isMaze)
+        {
+
+        }
+        else
+        {
+            
+        }
+    }
+
+
+    // Old Glowing
+    // void UseGlow()
+    // {
+    //     if (!isCooldown)
+    //     {
+    //         isCooldown = true;
+    //         glowAbilityTimerText.gameObject.SetActive(true);
+    //         glowButtonBorder.interactable = false;
+    //         glowButtonBackground.interactable = false;
+    //         glowCooldownTimer = glowCooldownTime;
+    //         abilityCooldownTimer = abilityCooldownTime;
+    //     }
+    // }
+
+    // void ApplyCooldown()
+    // {
+    //     abilityCooldownTimer -= Time.deltaTime;
+    //     glowCooldownTimer -= Time.deltaTime;
+
+    //     if (abilityCooldownTimer < 0.0f)
+    //     {
+    //         glowAbilityTimerText.gameObject.SetActive(false);
+    //         glowCooldownTimerText.gameObject.SetActive(true);
+            
+    //         if (glowCooldownTimer < 0.0f)
+    //         {
+    //             isCooldown = false;
+    //             glowCooldownTimerText.gameObject.SetActive(false);
+    //             glowButtonBorder.interactable = true;
+    //             glowButtonBackground.interactable = true;
+    //         }
+    //         else
+    //         {
+    //             glowCooldownTimerText.text = Mathf.RoundToInt(glowCooldownTimer).ToString();
+    //         }
+    //     }
+    //     else
+    //     {
+    //         glowAbilityTimerText.text = Mathf.RoundToInt(abilityCooldownTimer).ToString();
+    //     }   
+    // }
 }
