0faab40b77d48d256a16be6cece436857f78a739
TWalt97 <108764485+TWalt97@users.noreply.github.com>
2023-04-27 23:55:40 -0400

    adjusted fademanager script

--- Assets/FadeManager.cs
+++ Assets/FadeManager.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour
 {
     [SerializeField]
-    private UIFadeIn fadeIn;
+    private Image fadeImage;
 
-    void Awake()
+    [SerializeField]
+    private float fadeTime = 2f;
+    [SerializeField]
+    private float timeUntilFadeBack = 5f;
+
+    private void Start()
     {
-        fadeIn.FadeUI(5f);
+        StartCoroutine(Fade(timeUntilFadeBack, fadeTime));
+    }
+
+    IEnumerator Fade(float timeUntilFadeBack, float fadeTime)
+    {
+        float currentTime = 0;
+        while (currentTime < fadeTime)
+        {
+            currentTime += Time.deltaTime;
+            float fadeAlpha = Mathf.Lerp(1, 0, currentTime / fadeTime);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeAlpha);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(timeUntilFadeBack);
+
+        float newCurrentTime = 0;
+        while (newCurrentTime < fadeTime)
+        {
+            newCurrentTime += Time.deltaTime;
+            float fadeAlpha = Mathf.Lerp(0, 1, newCurrentTime / fadeTime);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, fadeAlpha);
+            yield return null;
+        }
+
+        yield return null;
     }
 }
